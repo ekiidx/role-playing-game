f8e1b7bf5c340951ba10349626da846768d3b1f4
ekiidx <info@vuedesign.co>
2023-11-06 01:18:45 -0500

    initial upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const canvas = document.querySelector('canvas')
+
+const c = canvas.getContext('2d')
+
+canvas.width = 1024
+canvas.height = 576
+
+c.fillStyle = 'white'
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+const image = new Image()
+image.src = './img'
+
+c.drawImage('./img')
